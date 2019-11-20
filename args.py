2b4f8c744d6d812cb8d72a998f3e6b7ee1c9b055
Dominik Schneider <dominik.schneider@wsu.edu>
2019-11-19 17:42:11 -0800

    add file for cmd args

---
+++ args.py
@@ -0,0 +1,11 @@
+
+class options():
+    def __init__(self):
+        self.config = "cppcserver-remote.config"
+        self.exper = "GoldStandard2"
+        self.outdir = "data"
+        self.date1 = "20190801"
+        self.date2 = "20190802"
+        self.force = True
+
+args = options()
