397701f67c0537efad56b7b1732dd46b055b9c10
Dominik Schneider <dominik.schneider@wsu.edu>
2019-11-22 14:31:50 -0800

    more changes than should be in 1 commit

--- args.py
+++ args.py
@@ -1,11 +1,25 @@
+import psycopg2
+import psycopg2.extras
+import argparse
+import json
+import os
+import sys
+import zipfile
+import paramiko
+import numpy as np
+import cv2
+import datetime
+import fnmatch
+from tqdm import tqdm
 
 class options():
     def __init__(self):
         self.config = "cppcserver-remote.config"
-        self.exper = "GoldStandard2"
-        self.outdir = "data"
-        self.date1 = "20190801"
-        self.date2 = "20190802"
+        self.exper = "GoldStandard2_RGB"
+        self.outdir = "testing"
+        self.date1 = "2019-08-01"
+        self.date2 = "2019-08-02"
         self.force = True
+        self.camera = None
 
 args = options()
